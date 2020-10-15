451f0f964d6867f8cf78e4eab3b2f9b38e0ff7cd
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2020-10-15 12:09:06 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <stdio.h>
 #include <Windows.h>
+#include <stdbool.h>
 
 using namespace std;
 
@@ -23,78 +24,73 @@ int main(){
     float PlayerFieldOfView = 16.0f;
     float PlayerWalkSpeed = 5.0f; /** Velocidade do jogador **/
 
-	// Create Screen Buffer
-	wchar_t *screen = new wchar_t[ScreenSizeOfWidth*ScreenSizeOfHeight];
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
+    bool PlayerStillAliveNotEndGame = true;
 
-	// Create Map of world space # = wall block, . = space
+    /** Buffer do Mapa **/
+	wchar_t *GameplayScreen = new wchar_t[ScreenSizeOfWidth*ScreenSizeOfHeight];
+	HANDLE GameConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(GameConsole);
+	DWORD DataOfBytesWritten = 0;
+
+    /** Mapa Cenario **/
 	wstring map;
 	map += L"#########.......";
 	map += L"#...............";
 	map += L"#.......########";
 	map += L"#..............#";
-	map += L"#......##......#";
-	map += L"#......##......#";
-	map += L"#..............#";
-	map += L"###............#";
+	map += L"#....####......#";
+	map += L"#.##...##...#..#";
+	map += L"#....#.........#";
+	map += L"###..#..###.#..#";
 	map += L"##.............#";
-	map += L"#......####..###";
-	map += L"#......#.......#";
-	map += L"#......#.......#";
+	map += L"#....#.####..###";
+	map += L"#....#.........#";
+	map += L"#.#..#.#.......#";
 	map += L"#..............#";
-	map += L"#......#########";
+	map += L"#..##..#####...#";
 	map += L"#..............#";
 	map += L"################";
 
-	auto tp1 = chrono::system_clock::now();
-	auto tp2 = chrono::system_clock::now();
+    /** Cronometro do Jogo **/
+	auto TimerPos1 = chrono::system_clock::now();
+	auto TimerPos2 = chrono::system_clock::now();
 
-	while (true){
-		// We'll need time differential per frame to calculate modification
-		// to movement speeds, to ensure consistant movement, as ray-tracing
-		// is non-deterministic
-		tp2 = chrono::system_clock::now();
-		chrono::duration<float> elapsedTime = tp2 - tp1;
-		tp1 = tp2;
+    /** Função Principal, Será executada enquando Jogador estiver vivo! **/
+	while (PlayerStillAliveNotEndGame){
+		TimerPos2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = TimerPos2 - TimerPos1;
+		TimerPos1 = TimerPos2;
 		float fElapsedTime = elapsedTime.count();
 
-
-		// Handle CCW Rotation
+		/** **/// Handle CCW Rotation
 		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
 			PlayerRotation -= (PlayerWalkSpeed * 0.75f) * fElapsedTime;
 
-		// Handle CW Rotation
+		/** **/// Handle CW Rotation
 		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
 			PlayerRotation += (PlayerWalkSpeed * 0.75f) * fElapsedTime;
 
-		// Handle Forwards movement & collision
-		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
-		{
+		/** **/// Handle Forwards movement & collision
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000){
 			PlayerXPosition += sinf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
 			PlayerYPosition += cosf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
-			if (map.c_str()[(int)PlayerXPosition * MapDimensionsWidth + (int)PlayerYPosition] == '#')
-			{
+			if (map.c_str()[(int)PlayerXPosition * MapDimensionsWidth + (int)PlayerYPosition] == '#'){
 				PlayerXPosition -= sinf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
 				PlayerYPosition -= cosf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
 			}
 		}
 
-		// Handle backwards movement & collision
-		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
-		{
+		/** **/// Handle backwards movement & collision
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000){
 			PlayerXPosition -= sinf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
 			PlayerYPosition -= cosf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
-			if (map.c_str()[(int)PlayerXPosition * MapDimensionsWidth + (int)PlayerYPosition] == '#')
-			{
+			if (map.c_str()[(int)PlayerXPosition * MapDimensionsWidth + (int)PlayerYPosition] == '#'){
 				PlayerXPosition += sinf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
 				PlayerYPosition += cosf(PlayerRotation) * PlayerWalkSpeed * fElapsedTime;;
 			}
 		}
 
-		for (int x = 0; x < ScreenSizeOfWidth; x++)
-		{
+		for (int x = 0; x < ScreenSizeOfWidth; x++){
 			// For each column, calculate the projected ray angle into world space
 			float fRayAngle = (PlayerRotation - PlayerSizeOfFov/2.0f) + ((float)x / (float)ScreenSizeOfWidth) * PlayerSizeOfFov;
 
@@ -110,23 +106,19 @@ int main(){
 
 			// Incrementally cast ray from player, along ray angle, testing for
 			// intersection with a block
-			while (!bHitWall && fDistanceToWall < PlayerFieldOfView)
-			{
+			while (!bHitWall && fDistanceToWall < PlayerFieldOfView){
 				fDistanceToWall += fStepSize;
 				int nTestX = (int)(PlayerXPosition + fEyeX * fDistanceToWall);
 				int nTestY = (int)(PlayerYPosition + fEyeY * fDistanceToWall);
 
 				// Test if ray is out of bounds
-				if (nTestX < 0 || nTestX >= MapDimensionsWidth || nTestY < 0 || nTestY >= MapDimensionsHeight)
-				{
+				if (nTestX < 0 || nTestX >= MapDimensionsWidth || nTestY < 0 || nTestY >= MapDimensionsHeight){
 					bHitWall = true;			// Just set distance to maximum depth
 					fDistanceToWall = PlayerFieldOfView;
 				}
-				else
-				{
+				else{
 					// Ray is inbounds so test to see if the ray cell is a wall block
-					if (map.c_str()[nTestX * MapDimensionsWidth + nTestY] == '#')
-					{
+					if (map.c_str()[nTestX * MapDimensionsWidth + nTestY] == '#'){
 						// Ray has hit wall
 						bHitWall = true;
 
@@ -138,9 +130,8 @@ int main(){
 
 						// Test each corner of hit tile, storing the distance from
 						// the player, and the calculated dot product of the two rays
-						for (int tx = 0; tx < 2; tx++)
-							for (int ty = 0; ty < 2; ty++)
-							{
+						for (int tx = 0; tx < 2; tx++){
+							for (int ty = 0; ty < 2; ty++){
 								// Angle of corner to eye
 								float vy = (float)nTestY + ty - PlayerYPosition;
 								float vx = (float)nTestX + tx - PlayerXPosition;
@@ -148,6 +139,7 @@ int main(){
 								float dot = (fEyeX * vx / d) + (fEyeY * vy / d);
 								p.push_back(make_pair(d, dot));
 							}
+						}
 
 						// Sort Pairs from closest to farthest
 						sort(p.begin(), p.end(), [](const pair<float, float> &left, const pair<float, float> &right) {return left.first < right.first; });
@@ -175,15 +167,13 @@ int main(){
 
 			if (bBoundary)		nShade = ' '; // Black it out
 
-			for (int y = 0; y < ScreenSizeOfHeight; y++)
-			{
+			for (int y = 0; y < ScreenSizeOfHeight; y++){
 				// Each Row
 				if(y <= nCeiling)
-					screen[y*ScreenSizeOfWidth + x] = ' ';
+					GameplayScreen[y*ScreenSizeOfWidth + x] = ' ';
 				else if(y > nCeiling && y <= nFloor)
-					screen[y*ScreenSizeOfWidth + x] = nShade;
-				else // Floor
-				{
+					GameplayScreen[y*ScreenSizeOfWidth + x] = nShade;
+				else{ /** piso **/
 					// Shade floor based on distance
 					float b = 1.0f - (((float)y -ScreenSizeOfHeight/2.0f) / ((float)ScreenSizeOfHeight / 2.0f));
 					if (b < 0.25)		nShade = '#';
@@ -191,25 +181,24 @@ int main(){
 					else if (b < 0.75)	nShade = '.';
 					else if (b < 0.9)	nShade = '-';
 					else				nShade = ' ';
-					screen[y*ScreenSizeOfWidth + x] = nShade;
+					GameplayScreen[y*ScreenSizeOfWidth + x] = nShade;
 				}
 			}
 		}
 
-		// Display Stats
-		//swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", PlayerXPosition, PlayerYPosition, PlayerRotation, 1.0f/fElapsedTime);
+		/** Mostra Informações sobre o Gameplay **/
+		swprintf_s(GameplayScreen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", PlayerXPosition, PlayerYPosition, PlayerRotation, 1.0f/fElapsedTime);
 
-		// Display Map
-		for (int nx = 0; nx < MapDimensionsWidth; nx++)
-			for (int ny = 0; ny < MapDimensionsWidth; ny++)
-			{
-				screen[(ny+1)*ScreenSizeOfWidth + nx] = map[ny * MapDimensionsWidth + nx];
-			}
-		screen[((int)PlayerXPosition+1) * ScreenSizeOfWidth + (int)PlayerYPosition] = 'P';
+		/** Mostra o Mapa do Jogo **/
+		for(int CoordXMap = 0; CoordXMap < MapDimensionsWidth; CoordXMap++){
+            for(int CoordYMap = 0; CoordYMap < MapDimensionsWidth; CoordYMap++)
+				GameplayScreen[(CoordYMap+1)*ScreenSizeOfWidth + CoordXMap] = map[CoordYMap * MapDimensionsWidth + CoordXMap];
+		}
+		GameplayScreen[((int)PlayerXPosition+1) * ScreenSizeOfWidth + (int)PlayerYPosition] = 'P';
 
-		// Display Frame
-		screen[ScreenSizeOfWidth * ScreenSizeOfHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, ScreenSizeOfWidth * ScreenSizeOfHeight, { 0,0 }, &dwBytesWritten);
+		/** Mostra Campo de visão do Jogador **/
+		char Aux_Screen = GameplayScreen[ScreenSizeOfWidth * ScreenSizeOfHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(GameConsole, Aux_Screen, ScreenSizeOfWidth * ScreenSizeOfHeight, { 0,0 }, &DataOfBytesWritten);
 	}
 
 	return 0;
